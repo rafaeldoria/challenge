ed6e9583cb1cca35e05fb7797ed8cab0e2ebe07f
Rafael Dória <ieieldoria@hotmail.com>
2019-10-06 18:00:45 -0300

    Ajustes public

--- public/js/app.js
+++ public/js/app.js
@@ -36909,12 +36909,43 @@ module.exports = function(module) {
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 $(document).ready(function () {
-  $(function () {
-    var availableTutorials = ["ActionScript", "Bootstrap", "C", "C++"];
-    $("#automplete-1").autocomplete({
-      source: availableTutorials
-    });
-  });
+  $('#timeline').click(function () {
+    $('.container').show();
+  }); // $("#strEvent").keyup(function (e) {
+  //     e.preventDefault();
+  //     if ($('#strEvent').val().length === 2) {
+  //         console.log($('#strEvent').val());
+  //         $.ajax({
+  //             url: '/events/autocomplete',
+  //             type: 'POST',
+  //             data: {
+  //                 'str': $('#strEvent').val()
+  //             },
+  //             datatype: 'json',
+  //             success: function (date) {
+  //                 console.log(data);
+  //             },
+  //             error: function (error) {
+  //                 console.log(error);
+  //             }
+  //         });
+  //         var options = {
+  //             data: ["var", "bu", "teste"]
+  //         }
+  //         $("#strEvent").easyAutocomplete(options);
+  //     }
+  // });
+  // $(function () {
+  //     var availableTutorials = [
+  //         "ActionScript",
+  //         "Bootstrap",
+  //         "C",
+  //         "C++",
+  //     ];
+  //     $("#automplete-1").autocomplete({
+  //         source: availableTutorials
+  //     });
+  // });
 });
 
 /***/ }),
